d5cfa8353239a8f92898c6434a330c10041e94fa
AdriannaC1114 <118206495+AdriannaC1114@users.noreply.github.com>
2022-12-02 13:23:27 -0600

    Add files via upload

---
+++ Adventofcode/Day2/Part1.ps1
@@ -0,0 +1,44 @@
+﻿$Strat = @(Get-content C:\users\clarke_Adrianna\Desktop\AdventofCode\day2\input.txt)
+<#
+A = Rock
+B = Paper
+C = Scissors 
+
+X = Rock
+Y = Paper
+Z = Scissors
+
+Loss = 0
+Draw = 3
+Win = 6
+
+#>
+$tournament = @()
+$Loss = 0 
+$Draw = 3
+$win = 6
+$x = 1
+$y = 2
+$z = 3
+Foreach($round in $strat){
+    $Array = @($round.split())
+    If(($Array[0] -eq "A") -and ($array[1] -eq "Y")){$tournament += $win + $y}
+    ElseIf(($Array[0] -eq "A") -and ($array[1] -eq "X")){$tournament += $draw + $x}
+    ElseIf(($Array[0] -eq "b") -and ($array[1] -eq "Y")){$tournament += $draw + $y}
+    ElseIf(($Array[0] -eq "b") -and ($array[1] -eq "Z")){$tournament += $win + $z}
+    ElseIf(($Array[0] -eq "c") -and ($array[1] -eq "Z")){$tournament += $draw + $Z}
+    ElseIf(($Array[0] -eq "c") -and ($array[1] -eq "X")){$tournament += $win + $X}
+    
+
+    ElseIf(($Array[0] -eq "a") -and ($array[1] -eq "Z")){$tournament += $Z}
+    ElseIf(($Array[0] -eq "b") -and ($array[1] -eq "X")){$tournament += $X}
+    ElseIf(($Array[0] -eq "c") -and ($array[1] -eq "Y")){$tournament += $Y}
+    
+    
+    }
+Foreach($round in $tournament){
+$Total += $round}
+$total
+
+clear-variable tournament
+clear-variable total
---
+++ Adventofcode/Day2/part2.ps1
@@ -0,0 +1,39 @@
+﻿$Strat = @(Get-content C:\users\clarke_Adrianna\Desktop\AdventofCode\day2\input.txt)
+<#
+A = Rock
+B = Paper
+C = Scissors 
+
+#>
+$tournament = @()
+$A = 1
+$B = 2
+$C = 3
+
+#lose
+$x = 0
+#draw
+$y = 3
+#win
+$z = 6
+Foreach($round in $strat){
+    $Array = @($round.split())
+    
+    If(($Array[0] -eq "A") -and ($array[1] -eq "Y")){$tournament += $Y + $A}
+    ElseIf(($Array[0] -eq "A") -and ($array[1] -eq "X")){$tournament += $X +$C}
+    ElseIf(($Array[0] -eq "a") -and ($array[1] -eq "Z")){$tournament += $Z + $B}
+    
+    ElseIf(($Array[0] -eq "b") -and ($array[1] -eq "Y")){$tournament += $Y + $B}
+    ElseIf(($Array[0] -eq "b") -and ($array[1] -eq "Z")){$tournament += $Z + $C}
+    ElseIf(($Array[0] -eq "b") -and ($array[1] -eq "X")){$tournament += $X + $A}
+    
+    ElseIf(($Array[0] -eq "c") -and ($array[1] -eq "Z")){$tournament += $Z + $A}
+    ElseIf(($Array[0] -eq "c") -and ($array[1] -eq "X")){$tournament += $X + $B}
+    ElseIf(($Array[0] -eq "c") -and ($array[1] -eq "Y")){$tournament += $Y + $C}
+    }
+Foreach($round in $tournament){
+$Total += $round}
+$total
+
+clear-variable tournament
+clear-variable total
