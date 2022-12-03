ce5562de5d401c738768660b2c39eeb9d2ad2e4a
AdriannaC1114 <118206495+AdriannaC1114@users.noreply.github.com>
2022-12-03 01:50:06 -0600

    Add files via upload

---
+++ Adventofcode/Day3/Part1.ps1
@@ -0,0 +1,72 @@
+﻿$global:rucksacks = @(get-content C:\Users\Adrianna\Desktop\input.txt)
+#Array for Values
+$CharacterDictionaryValue = @(
+    'BURNER'
+    'a'
+    'b'
+    'c'
+    'd'
+    'e'
+    'f'
+    'g'
+    'h'
+    'i'
+    'j'
+    'k'
+    'l'
+    'm'
+    'n'
+    'o'
+    'p'
+    'q'
+    'r'
+    's'
+    't'
+    'u'
+    'v'
+    'w'
+    'x'
+    'y'
+    'z'
+    'A'
+    'B'
+    'C'
+    'D'
+    'E'
+    'F'
+    'G'
+    'H'
+    'I'
+    'J'
+    'K'
+    'L'
+    'M'
+    'N'
+    'O'
+    'P'
+    'Q'
+    'R'
+    'S'
+    'T'
+    'U'
+    'V'
+    'W'
+    'X'
+    'Y'
+    'Z'
+    )
+Foreach($rucksack in $rucksacks){
+    $half = $rucksack.length /2
+    $Rucksack1 = $rucksack.Substring(0,$half)
+    $rucksack2 = $rucksack.Substring($half)
+    $rucksackArray1 = $rucksack1.ToCharArray()
+    $rucksackArray2 = $rucksack2.ToCharArray()
+    
+
+    $FindSame = compare-object $rucksackarray1 $rucksackarray2 -CaseSensitive -IncludeEqual -ExcludeDifferent | select-object -ExpandProperty Inputobject -First 1
+    $total += $CharacterDictionaryValue.IndexOf("$findsame")
+
+    }
+
+    clear-variable sum
+    clear-variable total
---
+++ Adventofcode/Day3/Part2.ps1
@@ -0,0 +1,87 @@
+﻿$global:rucksacks = @(get-content C:\Users\Adrianna\Desktop\input.txt)
+#Array for Values
+$CharacterDictionaryValue = @(
+    'BURNER'
+    'a'
+    'b'
+    'c'
+    'd'
+    'e'
+    'f'
+    'g'
+    'h'
+    'i'
+    'j'
+    'k'
+    'l'
+    'm'
+    'n'
+    'o'
+    'p'
+    'q'
+    'r'
+    's'
+    't'
+    'u'
+    'v'
+    'w'
+    'x'
+    'y'
+    'z'
+    'A'
+    'B'
+    'C'
+    'D'
+    'E'
+    'F'
+    'G'
+    'H'
+    'I'
+    'J'
+    'K'
+    'L'
+    'M'
+    'N'
+    'O'
+    'P'
+    'Q'
+    'R'
+    'S'
+    'T'
+    'U'
+    'V'
+    'W'
+    'X'
+    'Y'
+    'Z'
+    )
+$IndexNumber = 0
+#Create groups
+$start = 0 
+$end = 2
+$totalgroups = 0
+Do{
+$hash += @{$totalgroups = $rucksacks[$Start..$end]}
+$totalgroups += 1
+$start += 3
+$end += 3}
+Until($totalgroups -eq 100)
+
+
+#Find Values from each group
+Do{
+$Array = @($hash[$indexnumber])
+
+$rucksack1 = $array[0].ToCharArray()
+$rucksack2 = $array[1].ToCharArray()
+$rucksack3 = $array[2].ToCharArray()
+$round1 = compare-object $rucksack1 $rucksack2 -CaseSensitive -IncludeEqual -ExcludeDifferent | select-object -ExpandProperty Inputobject
+$findsame = Compare-Object $round1 $rucksack3 -CaseSensitive -IncludeEqual -ExcludeDifferent | select-object -ExpandProperty Inputobject -First 1
+$total += $CharacterDictionaryValue.IndexOf("$findsame")
+
+
+$indexnumber += 1
+}until($indexnumber -eq 100)
+
+$total
+
