ee3c824898280c6272b17c35cc9dd3711997e217
AdriannaC1114 <118206495+AdriannaC1114@users.noreply.github.com>
2022-12-05 00:05:12 -0600

    Add files via upload

---
+++ Adventofcode/Day4/Part1.ps1
@@ -0,0 +1,21 @@
+﻿$routes = @(Get-content C:\Users\Adrianna\Desktop\Day4\input)
+
+#Split Routes
+Foreach($route in $routes)
+{
+    $array = $route -split ","
+    $pathBuild1 = $array[0] -split "-" 
+    $path1 = $pathbuild1[0]..$pathbuild1[1]
+    $pathBuild2 = $array[1] -split "-" 
+    $path2 = $pathbuild2[0]..$pathbuild2[1]
+
+    $Compare = @(Compare-object $path1 $path2 -IncludeEqual -ExcludeDifferent)
+    $ArrayCompare = @($compare | select-object -expandproperty inputobject).Count
+
+        If(($ArrayCompare -eq $path2.count) -or ($arraycompare -eq $path1.count)){
+            $counter += 1 }
+            Else{$false}
+
+}
+$counter
+$counter = 0 
---
+++ Adventofcode/Day4/part2.ps1
@@ -0,0 +1,19 @@
+﻿$routes = @(Get-content C:\Users\Adrianna\Desktop\Day4\input)
+
+#Split Routes
+Foreach($route in $routes)
+{
+    $array = $route -split ","
+    $pathBuild1 = $array[0] -split "-" 
+    $path1 = $pathbuild1[0]..$pathbuild1[1]
+    $pathBuild2 = $array[1] -split "-" 
+    $path2 = $pathbuild2[0]..$pathbuild2[1]
+
+    $Compare = @(Compare-object $path1 $path2 -IncludeEqual -ExcludeDifferent).length
+    If($compare -gt 0){
+    $counter += 1}
+
+
+}
+$counter
+$counter = 0 
