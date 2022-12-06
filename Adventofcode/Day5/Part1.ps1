acca9c945ca2637a2cc55078873330f3a4cb2f2f
AdriannaC1114 <118206495+AdriannaC1114@users.noreply.github.com>
2022-12-05 19:07:44 -0600

    Update Part1.ps1

--- Adventofcode/Day5/Part1.ps1
+++ Adventofcode/Day5/Part1.ps1
@@ -1,17 +1,17 @@
-$MainStack = @(gc C:\users\clarke_Adrianna\Desktop\AdventofCode\Day5\input.txt)
+$MainStack = @(gc C:\Users\Adrianna\desktop\Day5\input.txt)
 
 #Build Cratestacks
 $CrateStacks = $mainstack[0..7]
 
-$1 = [System.Collections.ArrayList]@($cratestacks.Substring(1,1)  -ne ".") 
-$2 = [System.Collections.ArrayList]@($cratestacks.Substring(5,1) -ne ".")
-$3 = [System.Collections.ArrayList]@($cratestacks.Substring(9,1) -ne ".")
-$4 = [System.Collections.ArrayList]@($cratestacks.Substring(13,1) -ne ".")
-$5 = [System.Collections.ArrayList]@($cratestacks.Substring(17,1) -ne ".")
-$6 = [System.Collections.ArrayList]@($cratestacks.Substring(21,1)  -ne ".") 
-$7 = [System.Collections.ArrayList]@($cratestacks.Substring(25,1) -ne ".")
-$8 = [System.Collections.ArrayList]@($cratestacks.Substring(29,1) -ne ".")
-$9 = [System.Collections.ArrayList]@($cratestacks.Substring(33,1) -ne ".")
+$1 = @($cratestacks.Substring(1,1)  -ne ".") 
+$2 = @($cratestacks.Substring(5,1) -ne ".")
+$3 = @($cratestacks.Substring(9,1) -ne ".")
+$4 = @($cratestacks.Substring(13,1) -ne ".")
+$5 = @($cratestacks.Substring(17,1) -ne ".")
+$6 = @($cratestacks.Substring(21,1)  -ne ".") 
+$7 = @($cratestacks.Substring(25,1) -ne ".")
+$8 = @($cratestacks.Substring(29,1) -ne ".")
+$9 = @($cratestacks.Substring(33,1) -ne ".")
 $hash = @{}
 $hash.add(9,$9)
 $hash.add(1,$1)
@@ -36,13 +36,13 @@ $Instructions = Foreach($Line in $mainstack[10..550]){
         Else{$quantity = [int32]$line.substring(5,2)
              $source = [int32]$line.Substring(13,1)
              $Destination = [int32]$line.Substring(18,1)}
+            
+            #math time
+             $ArrayToFlip = @($hash.$source[0..($quantity-1)])
+             [Array]::Reverse($ArrayToFlip)
 
-        $hash.$source = $hash.$source[$quantity..1000]
-        
-        
-        Write-host $Line':'$hash.$source
-        Write-host $line':'$hash.$destination
-        clear-variable Dropin
-        clear-variable char
+
+            $hash.$destination = $ArrayToFlip + $hash.$destination
+            $hash.$source = $hash.$source[($quantity)..1000]
+
 }
-
