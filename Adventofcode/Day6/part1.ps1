c30d9ad3f745020e04d5edec05a112da444523ad
AdriannaC1114 <118206495+AdriannaC1114@users.noreply.github.com>
2022-12-05 23:37:50 -0600

    Add files via upload

---
+++ Adventofcode/Day6/part1.ps1
@@ -0,0 +1,11 @@
+﻿$code = gc C:\Users\Adrianna\Desktop\Day6\input
+
+Do{$MarkerCheck = $code.Substring($CharCount,4)
+$charcount++
+$markercheck
+}
+Until(($MarkerCheck.ToCharArray() | Sort-Object | Get-unique).count -eq 4 )
+
+$charcount + 4
+
+
---
+++ Adventofcode/Day6/part2.ps1
@@ -0,0 +1,12 @@
+﻿$code = gc C:\Users\Adrianna\Desktop\Day6\input
+
+Do{$MarkerCheck = $code.Substring($CharCount,14)
+$charcount++
+$markercheck
+}
+Until(($MarkerCheck.ToCharArray() | Sort-Object | Get-unique).count -eq 14 )
+
+write-host unique found $markercheck
+$charcount + 14
+
+clear-variable charcount
