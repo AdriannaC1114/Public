c8f44be4b7a80e3a7f4c5c7b828c4f3ca546608f
AdriannaC1114 <118206495+AdriannaC1114@users.noreply.github.com>
2022-12-09 18:04:48 -0600

    Create Part1.ps1

---
+++ Adventofcode/Day8/Part1.ps1
@@ -0,0 +1,18 @@
+$trees = @(gc C:\Users\clarke_Adrianna\Desktop\AdventofCode\Day8\input.txt)
+$visible = 0
+
+for($x = 0; $x -le $trees.Length; $x++)
+    {For($y = 0; $y -lt $trees[$x].Length; $y++)
+    {
+    
+    if(($y -eq 0) -or ($y -eq $trees.length -1) -or ($x -eq $trees.length -1) -or ($x -eq 0)){$visible++}
+    
+    #Find The Biggest Tree
+    #Define Initial Values
+    $up = 0
+    $down = 0 
+    $left = 0 
+    $right = 0
+    Else{
+    For($z=0; $z -lt $up.count)}
+    }}
